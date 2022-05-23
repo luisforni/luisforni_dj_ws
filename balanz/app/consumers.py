ebeec789ccc4cc263e4d28798554537e30530d80
Luis <70423935+luisforni@users.noreply.github.com>
2022-05-22 23:18:30 -0300

    Update consumers.py

--- balanz/app/consumers.py
+++ balanz/app/consumers.py
@@ -8,7 +8,7 @@ class WSConsumer(WebsocketConsumer):
     def connect(self):
         self.accept()
 
-        for i in range(10000):
+        for i in range(1000):
             self.send(json.dumps({'message': randint(1, 100)}))
             sleep(1)
 
